29594990256716806be3516b500db8b825a94256
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-06-23 02:08:51 +0530

    achivement start

--- src/components/Achivements/Achivements.jsx
+++ src/components/Achivements/Achivements.jsx
@@ -15,7 +15,21 @@ const Achivements = () => {
         </div>
       </div>
       <div className="body_information">
-
+        <div className="projects_section">
+            <div className="projects_section_heading">
+                <p>Projects</p>
+            </div>
+        </div>
+        <div className="certificates_section">
+        <div className="certificates_section_heading">
+                <p>certificates</p>
+            </div>
+        </div>
+        <div className="hackathon_section">
+        <div className="hackathon_section_heading">
+                <p>hackathon</p>
+            </div>
+        </div>
       </div>
     </div>
   )
